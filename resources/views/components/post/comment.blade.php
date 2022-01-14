bfb8d20ea41f0f68080306832973b4479394fbc7
Viezas <dxviezas@gmail.com>
2022-01-14 10:37:37 +0100

    fix comment like button

--- resources/views/components/post/comment.blade.php
+++ resources/views/components/post/comment.blade.php
@@ -6,8 +6,8 @@
             <div style="margin-right: 5%">
                 {{ __('user.profile_picture') }}
             </div>
-            <div>
-                <div class="w-100 d-flex align-items-center justify-content-between mb-2">
+            <div class="w-100">
+                <div class="w-25 d-flex align-items-center justify-content-between mb-2">
                     {{ $comment->user->name }} <span>{{ $comment->format_created_at }}</span>
                 </div>
                 <div class="mb-2">
@@ -15,7 +15,7 @@
                 </div>
                 <form action="{{ route('timeline.likeComment', $comment->id) }}" method="POST">
                     @csrf
-                    <button class="btn btn-outline-primary {{ $comment->liked() ? 'unlikeBtn bg-primary' : 'likeBtn' }}" style="width: 20%;">
+                    <button class="btn btn-outline-primary {{ $comment->liked() ? 'unlikeBtn bg-primary' : 'likeBtn' }}" style="width: 5%;">
                         @if ($comment->liked())
                             <svg xmlns="http://www.w3.org/2000/svg" fill="blue" fill-opacity="0%" viewBox="0 0 24 24" stroke="white">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 10h4.764a2 2 0 011.789 2.894l-3.5 7A2 2 0 0115.263 21h-4.017c-.163 0-.326-.02-.485-.06L7 20m7-10V5a2 2 0 00-2-2h-.095c-.5 0-.905.405-.905.905 0 .714-.211 1.412-.608 2.006L7 11v9m7-10h-2M7 20H5a2 2 0 01-2-2v-6a2 2 0 012-2h2.5" />
